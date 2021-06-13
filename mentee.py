46bff1d34de0d69453f354648870d256d133a9de
aaratee-123 <82765818+aaratee-123@users.noreply.github.com>
2021-06-13 22:24:23 +0530

    Add files via upload

---
+++ mentee.py
@@ -0,0 +1,34 @@
+print("open gmail application")
+print("checking information")
+option = input("enter the option")
+if option == "you":
+   print("create accont for myself")
+   new_accont=("enter your first name")
+   detail=("enter your last name")
+   if 'new_account' and detail:
+       print("it is wrong name")
+       if 'new_account' and detail:
+           print("it is right name")
+           a=input("enter your date of birth")
+           b=input("enter your gender")
+           print("cleck next")
+           create_account=("enter your gmail account")
+           if create_account:
+               print("saimmonu@gmail.com")
+           password=("enter your strong password")
+           if password== 'monu@2015':
+                 print("it is wrong password")
+           elif password=='saim@1995': 
+                 print("it is right password")
+                 add = "add phone number"
+                 terms = "terms and condition"
+                 print("add, terms")
+                 print("welcome to saimmonu@gmail.com")
+           else:
+             print("wrong password")
+       else:
+           print("blank colum")
+   else:
+        print("invailid gmail account")
+else:
+    print("give currect information")
